36c3bfb7813224613818b9c3ecbd844d7fed64c0
Admin <admin@MacBook-Pro.local>
2020-07-08 03:41:42 -0400

    add paypal method

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,32 @@ const IndexPage = () => {
   const { state, dispatch } = useContext(Context)
   const theme = useTheme()
 
+  const renderPaypal = () => {
+    paypal.Buttons({
+      style: {
+          shape: 'pill',
+          color: 'gold',
+          layout: 'horizontal',
+          label: 'pay',
+          tagline: true
+      },
+      createOrder: function(data, actions) {
+          return actions.order.create({
+              purchase_units: [{
+                  amount: {
+                      value: '10'
+                  }
+              }]
+          });
+      },
+      onApprove: function(data, actions) {
+          return actions.order.capture().then(function(details) {
+              alert('Transaction completed by ' + details.payer.name.given_name + '!');
+          });
+      }
+  }).render('#paypal-button-container');
+  }
+
   return (
     <Layout>
       <div
@@ -24,6 +50,9 @@ const IndexPage = () => {
           Switch
         </button>
         <h2>Dark mode id {state.isDark ? "on" : "off"}</h2>
+        <div id="paypal-button-container"></div>
+        <script src="https://www.paypal.com/sdk/js?client-id=sb&currency=USD" data-sdk-integration-source="button-factory"></script>
+        {renderPaypal()}
       </div>
     </Layout>
   )
